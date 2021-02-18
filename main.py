a43e80c2881b44d2ea25db103ddfbfc4919ff9b8
2mvang <marissa.vang@students.dominican.edu>
2021-02-18 15:12:44 -0800

    testing passing in attributes with GaryVee YT channel

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import requests
 import re
 from bs4 import BeautifulSoup as bs
 
-r = requests.get("https://champagneproxy.github.io/webscraping/example.html")
+r = requests.get("https://www.youtube.com/c/garyvee/videos")
 #print(r.content)
 
 soup = bs(r.content, features="html5lib")
 
-headers = soup.find_all(["h1", "h2"])
-print(headers)
+#headers = soup.find_all(["h1", "h2"])
+#print(headers)
+
+paragraph = soup.find_all("p", attrs={"id": "paragraph-id"})
+print(paragraph)
 
 #Pretty prints out our html
 #print(soup.prettify())
